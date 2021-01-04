57ed405c689a02e1c4cf530b968d44471afe5f6f
Bruno <brunodipaolo13@gmail.com>
2021-01-04 19:17:28 -0300

    Referenciando os postos do usuário

--- src/main/java/com/example/workshopmongo/domain/User.java
+++ src/main/java/com/example/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.example.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
@@ -14,6 +17,8 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {}
 
@@ -47,7 +52,13 @@ public class User implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	public List<Post> getPosts() {
+		return posts;
+	}
 
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -76,5 +87,6 @@ public class User implements Serializable {
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", email=" + email + "]";
-	}	
+	}
+
 }
